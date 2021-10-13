a9908df3badf04d7e809169ee1b4501244049a03
Brandon Evans <brandon.evans0708@gmail.com>
2021-10-12 18:47:26 -0600

    maybe this time

--- public/js/index.js
+++ public/js/index.js
@@ -105,7 +105,7 @@ function sendTransaction(isAdding) {
    }
 
    // add to beginning of current array of data
-   transactions.unshift(transaction);
+   transaction.unshift(transaction);
 
    // re-run logic to populate ui with new record
    populateChart();
